855e24ca5e775d2bf6cd4172d2a7caf54ec9668d
Maj <159164690+majtobijakodric@users.noreply.github.com>
2026-01-29 23:33:13 +0100

    Big change -> switched to TypeScript

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log('Server is running at http://localhost:' + port);
+});
