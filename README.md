3c39b5b22d8d58508f1529c473b44a1151bb4e6f
Maj Tobi Jakodric <159164690+majtobijakodric@users.noreply.github.com>
2026-01-30 12:53:03 -0800

    Add README instructions, and changed GET '/' request to serve index.html

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # AudioHub
+
+This is a free and open source webapp for listening music. It mostly written in TypeScript and ran in nodeJS.
+
+
+## How to install?
+To use these commands you need to have **nodeJS** and **git** installed on your system! 
+
+***(Windows)***
+Install nodeJS from https://nodejs.org/en/
+
+***(Linux)*** You should know how to do this
+
+
+```bash
+# Clone the repository
+git clone https://github.com/majtobijakodric/audiohub.git
+
+# Install TypeScript
+npm install typescript
+
+# Install dependencies
+npm install
+```
+
+## Run the app
+This will run the nodemon module that watches for any changes in ***src*** folder and refresh the page if any are made.
+```bash
+npm run dev 
+```
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
+// Import Express framework for building web servers
 import express from 'express';
+// Import type definitions for Request and Response objects
 import type { Request, Response } from 'express';
+import path from 'path';
 
+// Set the path to the public folder where static files are stored
+const publicDir = path.join(process.cwd(), 'src', 'public');
 const app = express();
-const port = 3001;
+const port = 8000;
 
+// Serve static files (HTML, CSS, JS, images, etc.) from the public folder
+app.use(express.static(publicDir));
+
+
+// Handle get requests
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(publicDir, 'index.html'));
 });
 
 app.get('/about', (req: Request, res: Response) => {
     res.send('This is AudioHub!');
+
 });
 
-
+// Start the server
 app.listen(port, () => {
     console.log('Server is running at http://localhost:' + port);
 });
+
