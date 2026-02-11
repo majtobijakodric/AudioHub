4dff38490bfaca4197c4bc83c0b2850e0f97bb7e
Maj <159164690+majtobijakodric@users.noreply.github.com>
2026-02-11 21:48:54 +0100

    Update secrets defaults

--- src/secrets.ts
+++ src/secrets.ts
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config({path: ".env"});
 
-export const PORT = process.env.PORT || 8000;
+export const PORT = process.env.PORT || 8080;
 export const DATABASE_URL = process.env.DATABASE_URL || "mysql://audiohub-user:123@localhost:3306/audiohub";
