ed06f76237238b1a6be96777666a23ff2fef3406
Maj <159164690+majtobijakodric@users.noreply.github.com>
2026-05-14 23:12:00 +0200

    Said hi to user in usermenu

--- frontend/js/userMenu.js
+++ frontend/js/userMenu.js
@@ -1,4 +1,14 @@
 const userMenu = document.getElementById("userMenu");
+const usernameDisplay = document.getElementById("username");
+
+async function setUsername() {
+  const username = await getName().then(name => {
+    if (name) {
+      return name;
+    }
+  });
+  usernameDisplay.textContent = "Hi, " + username;
+}
 
 // a function that toggles the user menu
 function toggleUserMenu() {
@@ -8,3 +18,19 @@ function toggleUserMenu() {
     userMenu.classList.add("hidden");
   }
 }
+
+async function getName() {
+  const res = await fetch("/getusername");
+
+  if (res.ok) {
+    const data = await res.json();
+    const username = data.username;
+    return username;
+  } else {
+    console.error("Failed to fetch username");
+  }
+}
+
+setUsername();
+
+
